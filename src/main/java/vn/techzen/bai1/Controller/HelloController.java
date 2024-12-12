f9e3502f4005a8551c3bc3ca961e3a363592b546
Your Name <you@example.com>
2024-12-12 08:52:00 +0700

    Hello

--- src/main/java/vn/techzen/bai1/Controller/HelloController.java
+++ src/main/java/vn/techzen/bai1/Controller/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class Hello {
+public class HelloController {
     @GetMapping("/hello")
     public String hello(@RequestParam(defaultValue = "") String name) {
         return String.format("Hello %s!!!", name);
