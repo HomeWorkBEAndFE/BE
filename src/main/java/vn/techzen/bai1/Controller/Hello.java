89f3cc97b9c32ab19368b658b259f54bff1650db
Your Name <you@example.com>
2024-12-12 08:50:09 +0700

    Hello

---
+++ src/main/java/vn/techzen/bai1/Controller/Hello.java
@@ -0,0 +1,13 @@
+package vn.techzen.bai1.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Hello {
+    @GetMapping("/hello")
+    public String hello(@RequestParam(defaultValue = "") String name) {
+        return String.format("Hello %s!!!", name);
+    }
+}
